d8ccc13456de52a90eb3ad77476bf47271b55ef8
Denys <denisbadakaty51@gmail.com>
2023-06-24 01:07:20 +0300

    Remoted extra code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './view/styles/index.scss';
 import CalculatorView from './view/calculatorView/CalculatorView';
-import calculate from './calculator/calculate/calculate';
 import Model from './model/Model';
 
 const model = new Model();
